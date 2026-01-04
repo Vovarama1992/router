2f015ba6abd1afa74280fdd0d472cdec9e59c8fa
Владимир Ушаков <volodzya13@Mac.dlink>
2026-01-04 08:34:07 +0300

    update

--- internal/openvpn/client.go
+++ internal/openvpn/client.go
@@ -12,70 +12,100 @@ type Client struct {
 	Config string
 }
 
+const (
+	pkiDir   = "/etc/openvpn/easy-rsa/pki"
+	caPath   = "/etc/openvpn/ca.crt"
+	tlsPath  = "/etc/openvpn/ta.key"
+	serverIP = "185.253.8.123"
+)
+
 func CreatePeer(name string) (*Client, error) {
 	log.Printf("[vpn] CreatePeer start: name=%s", name)
 
-	cmd := exec.Command(
-		"/etc/openvpn/easy-rsa/easyrsa",
-		"build-client-full",
-		name,
-		"nopass",
-	)
-	cmd.Dir = "/etc/openvpn/easy-rsa"
-	cmd.Env = append(os.Environ(), "EASYRSA_BATCH=1")
+	keyPath := pkiDir + "/private/" + name + ".key"
+	csrPath := pkiDir + "/reqs/" + name + ".csr"
+	certPath := pkiDir + "/issued/" + name + ".crt"
 
-	if out, err := cmd.CombinedOutput(); err != nil {
-		log.Printf("[vpn] easyrsa error (%s): %s", name, out)
-		return nil, fmt.Errorf("easy-rsa error: %w", err)
+	run := func(cmd *exec.Cmd) error {
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Printf("[vpn] cmd failed: %s\n%s", strings.Join(cmd.Args, " "), out)
+			return fmt.Errorf("cmd error: %w", err)
+		}
+		return nil
 	}
-	log.Printf("[vpn] easyrsa OK for %s", name)
+
+	// 1) private key
+	if err := run(exec.Command(
+		"openssl", "genrsa",
+		"-out", keyPath,
+		"2048",
+	)); err != nil {
+		return nil, err
+	}
+
+	// 2) CSR
+	if err := run(exec.Command(
+		"openssl", "req", "-new",
+		"-key", keyPath,
+		"-out", csrPath,
+		"-subj", "/CN="+name,
+	)); err != nil {
+		return nil, err
+	}
+
+	// 3) sign cert (PEM)
+	if err := run(exec.Command(
+		"openssl", "x509", "-req",
+		"-in", csrPath,
+		"-CA", caPath,
+		"-CAkey", pkiDir+"/private/ca.key",
+		"-CAcreateserial",
+		"-out", certPath,
+		"-days", "825",
+		"-sha256",
+	)); err != nil {
+		return nil, err
+	}
 
 	read := func(path string) ([]byte, error) {
-		log.Printf("[vpn] read file: %s", path)
 		b, err := os.ReadFile(path)
 		if err != nil {
-			log.Printf("[vpn] read FAILED: %s: %v", path, err)
 			return nil, err
 		}
-		log.Printf("[vpn] read OK: %s (%d bytes)", path, len(b))
 		return b, nil
 	}
 
-	ca, err := read("/etc/openvpn/ca.crt")
+	ca, err := read(caPath)
+	if err != nil {
+		return nil, err
+	}
+	cert, err := read(certPath)
+	if err != nil {
+		return nil, err
+	}
+	key, err := read(keyPath)
+	if err != nil {
+		return nil, err
+	}
+	tls, err := read(tlsPath)
 	if err != nil {
 		return nil, err
 	}
 
-	cert, err := read("/etc/openvpn/easy-rsa/pki/issued/" + name + ".crt")
-	if err != nil {
-		return nil, err
-	}
-
-	key, err := read("/etc/openvpn/easy-rsa/pki/private/" + name + ".key")
-	if err != nil {
-		return nil, err
-	}
-
-	tls, err := read("/etc/openvpn/ta.key")
-	if err != nil {
-		return nil, err
-	}
-
-	tplPath := "internal/configs/client.conf"
-	tpl, err := read(tplPath)
+	tpl, err := read("internal/configs/client.conf")
 	if err != nil {
 		return nil, err
 	}
 
 	cfg := string(tpl)
 	cfg = strings.ReplaceAll(cfg, "{{PEER_NAME}}", name)
-	cfg = strings.ReplaceAll(cfg, "{{SERVER_IP}}", "185.253.8.123")
+	cfg = strings.ReplaceAll(cfg, "{{SERVER_IP}}", serverIP)
 	cfg = strings.ReplaceAll(cfg, "{{CA}}", string(ca))
 	cfg = strings.ReplaceAll(cfg, "{{CERT}}", string(cert))
 	cfg = strings.ReplaceAll(cfg, "{{KEY}}", string(key))
 	cfg = strings.ReplaceAll(cfg, "{{TLS}}", string(tls))
 
 	log.Printf("[vpn] client config built: %d bytes", len(cfg))
-
 	return &Client{Config: cfg}, nil
 }
